57a9dbe144343030d72bfa0608a4643c62044009
THomas Blieberger <thomas.blieberger@hotmail.com>
2020-05-27 10:08:32 +0200

    finishes be6

---
+++ Thomas/be6.js
@@ -0,0 +1,13 @@
+var arr = [1, 2, 3, 100, 0.23, 2.74, 0.1, 7]
+
+function product(){
+	var i = 0
+	var prod = 1
+	for (arr[i]; i<arr.length; i++){
+		var prod = prod*arr[i]
+	}
+	return prod
+
+}
+var result = product()
+document.write(result)
