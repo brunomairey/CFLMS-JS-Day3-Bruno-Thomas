cb189fa59c550bde910733419a7a4b01aa46d9aa
brunomairey <bruno.mairey@yahoo.fr>
2020-05-27 10:35:56 +0200

    exo1

---
+++ basicB.js
@@ -0,0 +1,11 @@
+
+
+ 	function crystalGazer () {
+	var job_title = "Boss";
+	var location = "New Work";
+	var partners_name = "Carmen";
+	var number_childrens = 2;
+	var all =("You will be a " + job_title + " in " + location + " and married to " + partners_name + " with " + number_childrens + " childrens.");
+	return all
+	}
+	document.write(crystalGazer())
